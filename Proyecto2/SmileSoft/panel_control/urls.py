db49c8d603ff4774e5b01d59bb68e7691ec5568e
GloFlores <mercedesglo20@fpuna.edu.py>
2022-06-14 23:02:19 -0400

    creación de la plantilla de invitado, y las plantillas de tratamientos y contacto

--- Proyecto2/SmileSoft/panel_control/urls.py
+++ Proyecto2/SmileSoft/panel_control/urls.py
@@ -6,6 +6,15 @@ urlpatterns= [
     path ('inicio/', views.panel_control, name="panel_control"),
     path('error/', views.error, name="panel_control"),
     path('cambio_exitoso/', views.cambio_exitoso, name="panel_control"),
+    
+    path('registrarse/', views.registrese, name="registrarse"),
+    path('invitado/', views.invitado, name="panel_control"),
+    path('contacto/', views.contacto, name="panel_control"),
+    path('ortodoncia/', views.ortodoncia, name="ortodoncia"),
+    path('corona_dental/', views.corona_dental, name=" corona_dental"),
+    path('blanqueamiento/', views.blanqueamiento, name="blanqueamiento"),
+    path('extracciones/', views.extracciones, name="extracciones"),
+    
     # path('gestion_roles/', views.gestion_roles , name="gestion_roles"),
     # path('gestion_administrativo/', views.gestion_administrativo, name="gestion_administrativo"),
     # path('gestion_tratamiento/', views.gestion_tratamiento, name="gestion_tratamiento"),
