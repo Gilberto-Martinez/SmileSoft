38e7785dd4fa9344175676276f8f3adac49ca9a0
GloFlores <mercedesglo20@fpuna.edu.py>
2022-06-29 19:11:24 -0400

    cambios en agendamiento

--- Proyecto2/SmileSoft/gestion_agendamiento/urls.py
+++ Proyecto2/SmileSoft/gestion_agendamiento/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     # path("calendario/", views.CalendarView.as_view(), name="calendario"),
     path('calendario/', calendario, name="calendario"),
     path('agregar_cita/', agregar_cita, name="agregar_cita"),
+    path('modificar_cita/<str:numero_documento>', modificar_cita, name="modificar_cita"),
+    path('eliminar_cita/<str:numero_documento>',
+         eliminar_cita, name="eliminar_cita"),
 ]
