640e5a3f647c7fb1dd35a466a125a2f70097f4a7
Leslie Giselle <lesliefigueredo84@gmail.com>
2022-06-29 21:24:25 -0400

    Otros cambios de gestion_inventario_insumos
    otros cambios

--- Proyecto2/SmileSoft/gestion_inventario_insumos/models.py
+++ Proyecto2/SmileSoft/gestion_inventario_insumos/models.py
@@ -1,8 +1,27 @@
+from pyexpat import model
+from typing_extensions import Self
 from django.db import models
 from gestion_administrativo.models import *
 from gestion_inventario_insumos.models import *
 
 # Create your models here.
+
+
+#clase planilla
+#que tenga una lista del tipo insumo
+
+# class Inventario(models.Model):
+#     insumos=[]
+
+#     def cargar_insumos(self,insumo):
+#         self.insumos.append(insumo)
+
+#     def contar_insumos(self):
+#         cantidad = 0
+#         for insumo in self.insumos:
+#             cantidad = cantidad + 1
+#         return cantidad
+# de 1 a muchos
 
 class Insumo(models.Model):
     codigo_insumo= models.AutoField(primary_key=True, verbose_name='Código de tratamiento (*):')
@@ -10,9 +29,10 @@ class Insumo(models.Model):
     descripción_insumo= models.TextField(max_length=500, verbose_name='Descripción ():')
     precio=models.IntegerField(verbose_name='precio (*):')
     fecha_caducidad=models.DateField(verbose_name='Fecha de caducidad del insumo')
+    #id_inventario = models.ForeignKey(inventario)
 
         
-    # especialista = models.ForeignKey(EspecialistaSalud)
+  
     # LOAN_STATUS = (
     #     ('Disponible', 'Disponible'),
     #     ('Intermedio', 'Intermedio'),
@@ -29,3 +49,4 @@ class Insumo(models.Model):
     def __str__(self):
         return self.nombre_insumo
 
+#insumo con caducidad
