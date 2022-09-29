3158c0f8e92ef7951e398460c743a856b28a5dfa
GloFlores <mercedesglo20@fpuna.edu.py>
2022-09-28 20:01:11 -0400

    cambios en insumos y Insumos Asignados

--- Proyecto2/SmileSoft/gestion_inventario_insumos/urls.py
+++ Proyecto2/SmileSoft/gestion_inventario_insumos/urls.py
@@ -7,9 +7,11 @@ from .forms import *
 from gestion_tratamiento.views import PacienteList2
 urlpatterns = [
             path('listar_insumo/', listar_insumo, name="listar_tratamiento"),
-            path('eliminar_insumo/<nombre_insumo>/', eliminar_insumo, name="eliminar_insumo"),
+    path('eliminar_insumo/<codigo_insumo>/',
+                eliminar_insumo, name="eliminar_insumo"),
             path('agregar_insumo/', agregar_insumo, name="agregar_insumo"),
-            path('modificar_insumos/<nombre_insumo>/', modificar_insumo, name="modificar_insumos"),          
+            path('modificar_insumos/<codigo_insumo>/',
+                modificar_insumo, name="modificar_insumos"),
             path('listar_paciente2/', PacienteList2.as_view(),  name="listar_paciente2"),
             path('asignar_insumos/<int:codigo_tratamiento>', asignar_insumos, name="asignar_insumos"),
             path('listar_insumos_asignados/<codigo_tratamiento>', listar_insumo_asignado, name="listar_insumos_asignados"),
