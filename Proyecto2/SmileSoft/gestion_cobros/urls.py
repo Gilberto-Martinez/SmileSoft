6a9a6d9ce221f12ec423be4acd05ac56c9ff66fd
Gilberto-Martinez <57020462+Gilberto-Martinez@users.noreply.github.com>
2022-10-06 19:40:54 -0300

    Imolementacion de Cobros pendientes

--- Proyecto2/SmileSoft/gestion_cobros/urls.py
+++ Proyecto2/SmileSoft/gestion_cobros/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('cobrar_tratamiento/<int:id_paciente>', cobrar_tratamiento, name="cobrar_tratamiento"),
     path('registrar_cobro/<str:numero_documento>/', registrar_cobro, name="registrar_cobro"),
     path('listar_cobros/', listar_cobros, name="listar_cobros"),
+    path('listar_cobros_pendientes/', listar_cobros_pendientes, name="listar_cobros_pendientes"),
     # path('calendar/', pruebacalendar, name="calendar"),
     path('error_cobro/', ErrorCobro.as_view(), name="error_cobro"),
     path('mensaje_confirmacion_cobro/', ConfirmacionCobro.as_view(), name="mensaje_confirmacion_cobro"),
