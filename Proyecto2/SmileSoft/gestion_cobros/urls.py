1fc0da84ba5425e8fece19d722b146e196db1d31
Gilberto-Martinez <57020462+Gilberto-Martinez@users.noreply.github.com>
2022-11-10 00:08:42 -0300

    Inicio de implementacion de Facturación

--- Proyecto2/SmileSoft/gestion_cobros/urls.py
+++ Proyecto2/SmileSoft/gestion_cobros/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     path('listar_cobros_pendientes/', listar_cobros_pendientes, name="listar_cobros_pendientes"),
     path('eliminar_tratamiento_confirmado/<id_tratamiento_confirmado>', eliminar_tratamiento_confirmado, name="eliminar_tratamiento_confirmado"),
     path('error_cobro/', ErrorCobro.as_view(), name="error_cobro"),
-    path('mensaje_confirmacion_cobro/', ConfirmacionCobro.as_view(), name="mensaje_confirmacion_cobro"),
+    # path('mensaje_confirmacion_cobro/', ConfirmacionCobro.as_view(), name="mensaje_confirmacion_cobro"),
+    path('mensaje_confirmacion_cobro/<id_cobro>', mostrar_msj_confirmacion_cobro, name="mensaje_confirmacion_cobro"),
     path('ver_detalle_cobro/<int:id_cobro_contado>', ver_detalle_cobro, name="ver_detalle_cobro"),
     path('verificar_datos_cita/<numero_documento>/<menor_edad>', verificar_datos_cita, name="verificar_datos_cita"),
     path('solicitar_razon_social/<numero_documento>', solicitar_razon_social, name="solicitar_razon_social"),
     path('registrar_cobro2/<numero_documento>/<numero_documento2>/<razon_social>', registrar_cobro2, name="registrar_cobro2"),
+    path('ingresar_datos_factura/<id_cobro>', ingresar_datos_factura, name="ingresar_datos_factura"),
 ]
